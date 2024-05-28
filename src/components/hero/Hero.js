80220519ba9e79c2010ebe9f261b0841e3f6e606
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-05-28 02:15:28 -0400

    Offer Section.
    Just Started the offer section. Need to copy layout from hero section., then I'll provide the content later.

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Hero.module.css";
 import { Navigation } from "../../components";
 import { BackgroundPieceOne } from "../../assets/background";
+import heroImage from "../../assets/hero-image.png";
 
 const Hero = () => {
   return (
@@ -10,8 +11,19 @@ const Hero = () => {
         <BackgroundPieceOne />
       </div>
       <div className="container">
-        {/* navigation  */}
-        {/* hero content */}
+        <Navigation />
+        <div className={styles["hero-content"]}>
+          <h1>Small Business Web Design + Development</h1>
+          <p>
+            No page builders or WordPress—located in Maryland, we deliver 100%
+            custom-coded websites with exceptional results, starting at
+            $150/month.
+          </p>
+          <button className="btn-one">SEND A MESSAGE</button>
+        </div>
+        <div className={styles["hero-image"]}>
+          <img src={heroImage} alt="hero image" />
+        </div>
       </div>
     </section>
   );
