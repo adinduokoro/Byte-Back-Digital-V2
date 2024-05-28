ebb17c85f3c5c83260ab45460fd376fc33c2d452
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-05-28 04:06:11 -0400

    Updates
    Quick updates on the offer section.

--- src/components/whatWeOffer/WhatWeOffer.js
+++ src/components/whatWeOffer/WhatWeOffer.js
@@ -6,10 +6,16 @@ const WhatWeOffer = () => {
     <section className={styles["what-we-offer"]}>
       <div className="container">
         <div className={styles["what-we-offer-content"]}>
-          hello world
-          {/* h2 */}
+          <h2>
+            WHAT <span>WE OFFER</span>
+          </h2>
           {/* ruler */}
-          {/* p */}
+          <p className={styles["what-we-offer-description"]}>
+            We focus on crafting web design and development solutions for small
+            businesses across the US. Our hand-coded websites ensure optimal
+            performance and increased brand awareness, attracting more visitors,
+            and boosting your profits.
+          </p>
           {/* offer cards */}
         </div>
       </div>
