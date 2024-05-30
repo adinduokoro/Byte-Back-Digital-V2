e0a40fb88d7838bfb1aeaa337f3444c2cd2edd7d
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-05-29 22:57:10 -0400

    Payment Option section
    Importing dtat

---
+++ src/components/customerPlanOptions/data.js
@@ -0,0 +1,35 @@
+export const paymentOptions = [
+  {
+    title: "E-COMMERCE",
+    subTitle: "STARTING AT",
+    price: "5k",
+    perks: [
+      "$5000 min. up Front Cost",
+      "Custom Designed",
+      "Shopify or Stripe Integration",
+      "Admin Capabilities",
+    ],
+  },
+  {
+    title: "STANDARD",
+    subTitle: "5 PAGES",
+    price: "150",
+    perks: [
+      "Unlimited Edits",
+      "Includes Hosting",
+      "24/7 Customer Service",
+      "Lifetime Updates",
+    ],
+  },
+  {
+    title: "LOGOS & GRAPHIC DESIGN",
+    subTitle: "INCLUDED!",
+    price: "0",
+    perks: [
+      "3 revisions",
+      "All file Formats",
+      "Ready to use",
+      "FREE w/ Subscription",
+    ],
+  },
+];
