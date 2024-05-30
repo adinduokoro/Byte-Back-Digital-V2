1822a0292c8082ef6fde691e4cc132027c3f4c5d
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-05-29 20:52:27 -0400

    Blog Section
    Still working on setting this section up. Almost done.

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Blog.module.css";
 import { SectionTitle } from "../../components";
 import bookIcon from "../../assets/book-icon.svg";
+import { blogs } from "./data";
 
 const Blog = () => {
   return (
@@ -13,7 +14,16 @@ const Blog = () => {
             secondWord={"BLOG"}
             image={bookIcon}
           />
-          
+          <div className={styles.blogs}>
+            {blogs.map((blog, index) => (
+              <div className={styles["blog-card"]} key={index}>
+                <img src={blog.image} alt="" />
+                <h3>{blog.title}</h3>
+                <p>{blog.desc}</p>
+                <button className="btn-two">READ MORE</button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
--- src/components/blog/data.js
+++ src/components/blog/data.js
@@ -1,7 +1,21 @@
+import blog1 from "../../assets/blog-post- (1).jpg"
+import blog2 from "../../assets/blog-post- (2).jpg"
+import blog3 from "../../assets/blog-post- (3).jpg"
+
 export const blogs = [
     {
-        image: "",
+        image: blog1,
         title: "Are Custom Websites Better Than Builders?",
-        desc: "",
+        desc: "Discover how speed and security vary between the...",
+    },
+    {
+        image: blog2,
+        title: "Are Custom Websites Better Than Builders?",
+        desc: "Discover how speed and security vary between the...",
+    },
+    {
+        image: blog3,
+        title: "Are Custom Websites Better Than Builders?",
+        desc: "Discover how speed and security vary between the...",
     },
 ]
