4c8d9bb3ad383ac21245a801473048f20c8fd7d2
Addie Okoro <adinduokoro95@gmail.com>
2024-06-01 01:08:11 -0400

    The button is working properly.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import styles from "./Navigation.module.css";
 import { navLinks } from "./data";
 import Logo from "../../assets/logo";
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  SET_DARK_MODE,
-  selectIsDarkModeOn,
-} from "../../redux/slice/themeSlice";
+import { SET_DARK_MODE, selectIsDarkModeOn } from "../../redux/slice/themeSlice";
 
 const Navigation = () => {
   const dispatch = useDispatch();
@@ -26,22 +23,18 @@ const Navigation = () => {
           </div>
           <div className={styles["nav-buttons"]}>
             <ul className={styles["nav-links"]}>
-              {navLinks.map((link, index) => {
-                return (
-                  <li className="label-text" key={index}>
-                    <Link to={link.path}>{link.name}</Link>
-                  </li>
-                );
-              })}
+              {navLinks.map((link, index) => (
+                <li className="label-text" key={index}>
+                  <Link to={link.path}>{link.name}</Link>
+                </li>
+              ))}
             </ul>
             <button
-              className={` ${styles["top-toggle-button"]} toggle-container`}
+              className={`${styles["top-toggle-container"]} toggle-container`}
               onClick={handleToggle}
             >
-              <span className="toggler-container">
-                <span
-                  className={`toggler ${isDarkModeOn ? "toggler-dark" : ""}`}
-                ></span>
+              <span className={`toggler-container ${isDarkModeOn ? "toggler-container-dark" : ""}`}>
+                <span className={`toggler ${isDarkModeOn ? "toggler-dark" : ""}`}></span>
               </span>
             </button>
           </div>
