52b7483ba7d90370ef7de7623cc3602e865fc9d7
Addie Okoro <adinduokoro95@gmail.com>
2024-06-02 05:08:40 -0400

    Color Variables
    Update

--- src/components/customerPlanOptions/CustomerPlanOptions.js
+++ src/components/customerPlanOptions/CustomerPlanOptions.js
@@ -2,16 +2,27 @@ import React from "react";
 import styles from "./CustomerPlanOptions.module.css";
 import { paymentOptions } from "./data";
 import checkmark from "../../assets/checkmark-outline.svg";
+import { useSelector } from "react-redux";
+import { selectIsDarkModeOn } from "../../redux/slice/themeSlice";
 
 const CustomerPlanOptions = () => {
+  const isDarkModeOn = useSelector(selectIsDarkModeOn);
+
   return (
     <section className={styles["customer-plan-options"]}>
       <div className="container">
         <div className={styles["customer-plan-options-content"]}>
           <div className={styles["plan-options"]}>
             {paymentOptions.map((option, index) => (
-              <div className={styles["plan"]} key={index}>
-                <div className={styles["plan-title-container"]}>
+              <div
+                className={`${styles["plan"]} ${
+                  isDarkModeOn ? styles["plan-dark"] : ""
+                } `}
+                key={index}
+              >
+                <div className={`${styles["plan-title-container"]} ${
+                  isDarkModeOn ? styles["plan-title-container-dark"] : ""
+                } `}>
                   <h2>{option.title}</h2>
                 </div>
                 <span>{option.subTitle}</span>
