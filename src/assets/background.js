302bba7edb6059393ea88fb1f41c7adcf48d6935
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-06-18 02:14:22 -0400

    Updates

--- src/assets/background.js
+++ src/assets/background.js
@@ -39,7 +39,7 @@ const BackgroundPieceTwo = () => {
       xmlns="http://www.w3.org/2000/svg"
       style={{
         position: "absolute",
-        top: "0",
+        top: "-2",
         left: "0",
         minWidth: "90rem",
         minHeight: "49.49056rem",
