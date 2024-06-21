4b045e1f04c4f304f76991f6a9d6ae82a84df5bd
Addie Okoro <adinduokoro95@gmail.com>
2024-06-21 03:42:16 -0400

    Quality Quantity section

--- src/components/qualityQuantity/QualityQuantity.js
+++ src/components/qualityQuantity/QualityQuantity.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import styles from "./QualityQuantity.module.css"
+import React from "react";
+import styles from "./QualityQuantity.module.css";
 
 const QualityQuantity = () => {
   return (
     <section className={styles["quality-quantity"]}>
-      <div className='container'>
+      <div className="container">
         <div className={styles["quality-quantity-content"]}>
+          <h2>Crafting Your Digital Identity</h2>
+          <p>
+            Crafting your digital identity is our focus. We prioritize quality,
+            tailoring every aspect to reflect your unique story and values. With
+            a collaborative approach, we ensure your website embodies your
+            vision, resonating with your audience.
+          </p>
+          <div className={styles["card-container"]}>
 
+          </div>
+          <button>Our Services</button>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default QualityQuantity
+export default QualityQuantity;
---
+++ src/components/qualityQuantity/data.js
@@ -0,0 +1,17 @@
+export const card = [
+  {
+    id: "1",
+    title: "No Pre-Written Content",
+    desc: "We write unique and engaging content around your company. Everyone is unique!",
+  },
+  {
+    id: "2",
+    title: "",
+    desc: "",
+  },
+  {
+    id: "3",
+    title: "",
+    desc: "",
+  },
+];
