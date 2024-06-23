403261a2f6aadbfad21d818748d212f26ed3a9d6
Addie Okoro <adinduokoro95@gmail.com>
2024-06-23 16:03:06 -0400

    About Contact Section

--- src/components/aboutContact/AboutContact.js
+++ src/components/aboutContact/AboutContact.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./AboutContact.module.css";
-import {ContactForm} from "../../components"
+import { ContactForm } from "../../components";
+import PhoneIcon from "../../assets/phone-icon";
+import PlaneIcon from "../../assets/plane-icon";
 
 const AboutContact = () => {
   return (
@@ -8,16 +10,29 @@ const AboutContact = () => {
       <div className="container">
         <div className={styles["about-contact-content"]}>
           <div className={styles.left}>
-            {/* contact details */}
+            <div className={styles["contact-card"]}>
+              <div className={styles.card}>
+                <div className={styles["icon-container"]}>
+                  <PhoneIcon />
+                </div>
+                <div className={styles["phone-details"]}>
+                  <h2>Phone:</h2>
+                  <p>(410) 870 5675</p>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <div className={styles["icon-container"]}>
+                  <PlaneIcon />
+                </div>
+                <div className={styles["email-details"]}>
+                  <h2>Email:</h2>
+                  <p>bytebackdigital@gmail.com</p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className={styles.right}>
             <ContactForm />
-            <div className={styles["contact-card"]}>
-              <div className={styles.card}>
-                {/* icon */}
-                {/* phone */}
-              </div>
-            </div>
           </div>
         </div>
       </div>
