c01227ce5b44590f08103b13e3bfa9208be03a23
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-06-24 03:07:10 -0400

    Updates

--- src/components/serviceUpdates/ServiceUpdates.js
+++ src/components/serviceUpdates/ServiceUpdates.js
@@ -5,7 +5,22 @@ const ServiceUpdates = () => {
   return (
     <section className={styles["service-updates"]}>
       <div className="container">
-        <div className={styles["service-updates-content"]}>hello world</div>
+        <div className={styles["service-updates-content"]}>
+          <div className={styles.left}>
+            <h2>Lifetime Updates for Your Website</h2>
+            <p>
+              Google periodically updates its search algorithm, shifting the
+              criteria for ranking. Additionally, web accessibility guidelines
+              for screen readers are regularly revised. Therefore, we provide
+              <span>lifetime updates</span> for your website to ensure it remains current and
+              evolves with the latest standards.
+            </p>
+            <button>Learn More</button>
+          </div>
+          <div className={styles.right}>
+            {/* cards */}
+          </div>
+        </div>
       </div>
     </section>
   );
---
+++ src/components/serviceUpdates/data.js
@@ -0,0 +1,18 @@
+export const card = [
+  {
+    title: "Design Updates",
+    desc: "If you need a fresh design, we'll revamp your website.",
+  },
+  {
+    title: "Digital Accessibility",
+    desc: "Keeping up with the latest standards for web accessibility compliance.",
+  },
+  {
+    title: "Search Optimization",
+    desc: "Your website will remain current with Google's ranking guidelines.",
+  },
+  {
+    title: "Google Business Listing",
+    desc: "We also assist in optimizing and managing your business profiles on Google, Bing, and Yahoo.",
+  },
+];
