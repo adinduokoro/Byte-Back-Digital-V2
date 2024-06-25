d4fb2eebfa27694a82263114f72bb2c095b3dd2a
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2024-06-24 22:07:31 -0400

    Worked on some of the service page. Not feeling it today.

--- src/components/serviceUpdates/ServiceUpdates.js
+++ src/components/serviceUpdates/ServiceUpdates.js
@@ -1,7 +1,13 @@
 import React from "react";
 import styles from "./ServiceUpdates.module.css";
+import { cards } from "./data";
+import { useSelector } from "react-redux";
+import { selectIsDarkModeOn } from "../../redux/slice/themeSlice";
+import { Link } from "react-router-dom";
 
 const ServiceUpdates = () => {
+  const isDarkModeOn = useSelector(selectIsDarkModeOn);
+
   return (
     <section className={styles["service-updates"]}>
       <div className="container">
@@ -11,14 +17,28 @@ const ServiceUpdates = () => {
             <p>
               Google periodically updates its search algorithm, shifting the
               criteria for ranking. Additionally, web accessibility guidelines
-              for screen readers are regularly revised. Therefore, we provide
-              <span>lifetime updates</span> for your website to ensure it remains current and
-              evolves with the latest standards.
+              for screen readers are regularly revised. Therefore, we provide 
+              <span> lifetime updates</span> for your website to ensure it
+              remains current and evolves with the latest standards.
             </p>
-            <button>Learn More</button>
+            <Link to="/contact">
+              <button
+                className={`btn-one ${isDarkModeOn ? "btn-one-dark" : ""}`}
+              >
+                Learn More
+              </button>
+            </Link>
           </div>
           <div className={styles.right}>
-            {/* cards */}
+            {cards.map((card, index) => {
+              return (
+                <div className={styles.card} key={index}>
+                  {/* icon */}
+                  <h3>{card.title}</h3>
+                  <p>{card.desc}</p>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
--- src/components/serviceUpdates/data.js
+++ src/components/serviceUpdates/data.js
@@ -1,17 +1,21 @@
-export const card = [
+export const cards = [
   {
+    icon: "",
     title: "Design Updates",
     desc: "If you need a fresh design, we'll revamp your website.",
   },
   {
+    icon: "",
     title: "Digital Accessibility",
     desc: "Keeping up with the latest standards for web accessibility compliance.",
   },
   {
+    icon: "",
     title: "Search Optimization",
     desc: "Your website will remain current with Google's ranking guidelines.",
   },
   {
+    icon: "",
     title: "Google Business Listing",
     desc: "We also assist in optimizing and managing your business profiles on Google, Bing, and Yahoo.",
   },
